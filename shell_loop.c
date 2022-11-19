ca06709f3c90207625d3b1f4cca3d2c9c251893e
akinsiraifedayo <akinsiraolympicson@gmail.com>
2022-11-18 22:11:51 -0500

    edited the shell_loop.c

--- shell_loop.c
+++ shell_loop.c
@@ -2,7 +2,7 @@
 
 /**
  * hsh - main shell loop
- * @info: the parameter & return info struct
+ * @info: the accepted parameter and returns err
  * @av: the argument vector from main()
  *
  * Return: 0 on success, 1 on error, or error code
